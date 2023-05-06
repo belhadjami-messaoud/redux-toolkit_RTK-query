e026da4829fcd4e938d88dfb1a124c270a46459b
calem48 <messaoud.messaoud92@gmail.com>
2023-05-06 12:02:45 +0100

    edit at auth

--- src/pages/Dashborad/ShareLayoat.jsx
+++ src/pages/Dashborad/ShareLayoat.jsx
@@ -1,25 +1,26 @@
-
-import Navbar from '../../component/navbar/navbar'
-import { Outlet, Link } from 'react-router-dom'
+import { useEffect } from "react";
+import Navbar from "../../component/navbar/navbar";
+import { Outlet, Link } from "react-router-dom";
 export default function Home() {
-    const style = {
-
-    }
-    return (
-
-        <>
-            <Navbar />
-            <div className="container" style={{ display: "flex", }}  >
-                <aside style={{ width: "40%", height: "85vh", background: "#ccc" }}>
-                    <div> <Link to="/dash/home" >home</Link> </div>
-                    <div> <Link to="/dash/profile" >profile</Link> </div>
-                    <div> <Link to="/dash/addProduct" >add product</Link> </div>
-                </aside>
-                <div className="dashbord" style={{ width: "60%", }}>
-                    <Outlet />
-                </div>
-            </div>
-        </>
-
-    )
+  return (
+    <>
+      <Navbar />
+      <div className="container" style={{ display: "flex" }}>
+        <aside style={{ width: "40%", height: "85vh", background: "#ccc" }}>
+          <div>
+            <Link to="/dash/home">home</Link>{" "}
+          </div>
+          <div>
+            <Link to="/dash/profile">profile</Link>{" "}
+          </div>
+          <div>
+            <Link to="/dash/addProduct">add product</Link>{" "}
+          </div>
+        </aside>
+        <div className="dashbord" style={{ width: "60%" }}>
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 }
