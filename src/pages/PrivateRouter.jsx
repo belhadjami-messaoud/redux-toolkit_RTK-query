dec60f9a43fb6bb61f5f7ef94c14474e6a7d9413
calem48 <messaoud.messaoud92@gmail.com>
2023-05-06 12:02:08 +0100

    edit at auth

--- src/pages/PrivateRouter.jsx
+++ src/pages/PrivateRouter.jsx
@@ -1,39 +1,34 @@
+import { Navigate, useNavigate, BrowserRouter } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-import { Navigate, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useUserRefreshMutation } from "../services/auth/authAPI";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { reset, userRefresh } from "../features/authSlice";
 
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useUserRefreshMutation } from '../services/auth/authAPI';
-import { Outlet } from 'react-router-dom';
-import { useState } from 'react';
-import { userRefresh } from '../features/authSlice';
+export default function PrivateRouter({ children, auth }) {
+  const { isSuccess, isLoading, user, isAuthenticated } = useSelector(
+    (store) => store.auth
+  );
+  const location = useLocation();
+  const dispatch = useDispatch();
 
-export default function PrivateRouter({ children }) {
-    const { user } = useSelector(store => store.auth)
+  //   useEffect(() => {
+  //     dispatch(userRefresh());
+  //   }, [location]);
+  //   console.log(auth);
 
-    const [userRefresh, { isSuccess, isLoading }] = useUserRefreshMutation()
-    useEffect(() => {
-        if (user?.isAuthenticated) {
-            userRefresh()
-            console.log("userRefresh")
-        }
+  //   if (isLoading) {
+  //     return <div>loding...........................</div>;
+  //   }
 
-    }, [])
-
-
-
+  if (auth) {
     return (
-        <>
-            {
-
-                user && user?.role === "admin" ?
-                    children
-                    : <Navigate to="/" />
-            }
-        </>
-    )
-
+      <>{auth && auth.role === "admin" ? children : <Navigate to="/" />}</>
+    );
+  }
 }
 // return user && user?.role === "admin" ?//go to dash
 // children : user?.role === "user" ? // go to home if he is user else go to login
