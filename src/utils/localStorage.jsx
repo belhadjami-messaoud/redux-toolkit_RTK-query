3373a64cf6f5d03c93b77d5070469a31d7050c5b
calem48 <messaoud.messaoud92@gmail.com>
2023-05-06 12:04:01 +0100

    add func get  cookie

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -1,15 +1,15 @@
-
 export const addUserLocalStorage = (user) => {
-    return localStorage.setItem("user", JSON.stringify(user))
-}
-
-
+  return localStorage.setItem("user", JSON.stringify(user));
+};
 
 export const removeUserLocalStorage = () => {
-    return localStorage.removeItem("user")
-}
-
+  return localStorage.removeItem("user");
+};
 
 export const getUserLocalStorage = () => {
-    return JSON.parse(localStorage.getItem("user")) || null
-}
+  return JSON.parse(localStorage.getItem("user")) || null;
+};
+
+export const getUserCookie = () => {
+  document.cookie.split("=")[1] ? true : false;
+};
