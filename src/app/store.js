f645543f23d350d53a9547c12b7bda8d1bd79ff5
calem48 <messaoud.messaoud92@gmail.com>
2023-05-06 11:59:01 +0100

    comment rtk query

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 // import productSlice from '../features/productSlice'
-import { baseAPI } from '../services/api/baseAPI'
-import authSlice from '../features/authSlice'
-
-
-
+import { baseAPI } from "../services/api/baseAPI";
+import authSlice from "../features/authSlice";
 
 export const store = configureStore({
-    reducer: {
-        [baseAPI.reducerPath]: baseAPI.reducer,
-        auth: authSlice
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseAPI.middleware),
-    devTools: true
-})
+  reducer: {
+    // [baseAPI.reducerPath]: baseAPI.reducer,
+    auth: authSlice,
+  },
+  //   middleware: (getDefaultMiddleware) =>
+  //     getDefaultMiddleware().concat(baseAPI.middleware),
 
+  devTools: true,
+});
